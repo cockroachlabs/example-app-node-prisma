fb7f9d03a27178446e33ed1d6419c92e8d1d62bf
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-01-13 13:53:59 -0500

    App refactor
    - Added new "Customer" table to database schema
    - Added FK column to "Account" table, with ON DELETE CASCADE option
    - Removed transaction retry wrapper
    - Updated business logic to be more terse
    - Updated node package file to be more complete, accurate
    - Updated Prisma schema file with new database schema

--- dbinit.sql
+++ dbinit.sql
@@ -1,4 +1,9 @@
+CREATE TABLE "Customer" (
+    id UUID PRIMARY KEY
+);
+
 CREATE TABLE "Account" (
     id UUID PRIMARY KEY,
+    customer_id UUID REFERENCES "Customer"("id") ON DELETE CASCADE,
     balance INT8
 );
--- index.js
+++ index.js
@@ -3,44 +3,24 @@ const { v4: uuidv4 } = require('uuid')
 
 const prisma = new PrismaClient()
 
-// retryTxn wraps SQL operations in an explicit transaction.
-// If the transaction fails, the function retries the operation, with exponential backoff.
-async function retryTxn (n, max, operation) {
-  while (true) {
-    n++
-    if (n === max) {
-      throw new Error('Max retry count reached.')
-    }
-    try {
-      await operation()
-      await prisma.$queryRaw`COMMIT;`
-      return
-    } catch (err) {
-      if (err.code !== '40001') { // Transaction retry errors use the SQLSTATE error code 40001 (serialization error)
-        return err
-      } else {
-        console.log('Transaction failed. Retrying transaction.')
-        console.log(err.message)
-        console.log('Rolling back transaction.')
-        await prisma.$queryRaw`ROLLBACK;`
-        await new Promise((resolve) => setTimeout(resolve, 2 ** n * 1000))
-      }
-    }
-  }
-}
+main = async () => {
+  const customerIds = Array(10).fill().map(() => ({ id: uuidv4() }))
+  const accountValues = Array(10).fill().map((_, index) => ({ id: uuidv4(), customer_id: customerIds[index].id, balance: Math.floor(Math.random() * 1000) }))
 
-// initTable inserts the input row values into the account table.
-async function initTable (rowVals) {
-  await prisma.account.createMany({
-    data: rowVals,
-    skipDuplicates: true
+  const insertCustomerRows = await prisma.customer.createMany({
+    data: customerIds
   })
-  console.log('Account rows added.')
-}
 
-// updateTable updates existing row values in the account table.
-async function updateTable () {
-  await prisma.account.updateMany({
+  console.log('Customer rows inserted.', insertCustomerRows)
+
+  const insertAccountRows = await prisma.account.createMany({
+    data: accountValues
+  })
+
+  console.log('Account rows inserted.', insertAccountRows)
+  console.log('Initial Account row values:\n', await prisma.account.findMany())
+
+  const updateRows = await prisma.account.updateMany({
     where: {
       balance: {
         gt: 100
@@ -52,44 +32,13 @@ async function updateTable () {
       }
     }
   })
-  console.log('Account rows updated.')
-}
 
-// deleteRows deletes all rows in the account table.
-async function deleteRows () {
-  const deleteAllRows = await prisma.account.deleteMany()
-  return console.log('Account rows deleted.', deleteAllRows)
-}
+  console.log('Account rows updated.', updateRows)
+  console.log('Updated Account row values:\n', await prisma.account.findMany())
 
-// genVals generates random UUID values and integer values to be inserted into the account table.
-async function genVals (n) {
-  const accountValues = Array(n)
-  let i = 0
-  while (i < accountValues.length) {
-    const idVal = await uuidv4()
-    const balVal = await Math.floor(Math.random() * 1000)
-    accountValues[i] = { id: idVal, balance: balVal }
-    i++
-  }
-  return accountValues
-}
+  const deleteAllRows = await prisma.customer.deleteMany()
 
-async function main () {
-  const firstAccount = await genVals(10)
-
-  await retryTxn(0, 15, initTable(firstAccount))
-
-  await new Promise(resolve => setTimeout(resolve, 2000)) // wait 2 seconds before selecting and printing rows
-  const firstRows = await prisma.account.findMany()
-  console.log('Initial row values:\n', firstRows)
-
-  await retryTxn(0, 15, updateTable)
-
-  await new Promise(resolve => setTimeout(resolve, 2000)) // wait 2 seconds before selecting and printing rows
-  const updatedRows = await prisma.account.findMany()
-  console.log('Updated row values:\n', updatedRows)
-
-  await retryTxn(0, 15, deleteRows)
+  console.log('All Customer rows deleted.', deleteAllRows)
 }
 
 main()
